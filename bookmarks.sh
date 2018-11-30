ca2f6772913df1697a87876626776b4ba7a1b13b
v3x4t0r <vexator@agent-orange>
2018-11-30 08:34:41 +0000

    added functions

--- bookmarks.sh
+++ bookmarks.sh
@@ -8,53 +8,26 @@ then
     rm /var/lib/dpkg/lock
   fi
 
-  #Check if build-essentials is installed
-  build=$(dpkg -l | grep build-essential)
-  if [ -z "$build" ]
-  then
-    echo "Build-Essential Not installed, install it? y/n"
-    read install
-    var=y
-    if [ $install = $var ]
+  function install() {
+    toinstall=$(dpkg -l | grep "$1")
+    if [ -z "$toinstall" ]
     then
-      sudo apt install build-essential
-    else exit
-    fi
+      echo "$1 is not installed, install it now? (y/n)"
+      read install
+      var=y
+      if [ $install = $var ]
+      then
+        sudo apt install $1
+      fi
     else
-      echo "Build-Essentail is installed"
-  fi
+      echo "$1 is installed"
+    fi
+  }
 
-  #Check if liblz4-dev is installed
-  lz4=$(dpkg -l | grep liblz4-dev)
-  if [ -z "$lz4" ]
-  then
-    echo "liblz4-dev = Not installed, install it? y/n"
-    read install1
-    var=y
-    if [ $install1 = $var ]
-    then
-      sudo apt install liblz4-dev
-    else exit
-    fi
-    else
-      echo "liblz4-dev is installed"
-  fi
+install build-essential
+install liblz4-dev
+install pkg-config
 
-  #Check if pkg-config is installed
-  pkg=$(dpkg -l | grep pkg-config)
-  if [ -z "$pkg" ]
-  then
-    echo "pkg-config = Not installed, install it? y/n"
-    read install2
-    var=y
-    if [ $install2 = $var ]
-    then
-      sudo apt install pkg-config
-    else exit
-    fi
-    else
-      echo "liblz4-dev is installed"
-  fi
 
   #Find path of lz4jsoncat if compiled
   lz4jsoncat=$(sudo find / -name 'lz4jsoncat' 2> /dev/null)
